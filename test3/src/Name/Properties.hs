2d73fa1ac62e2e3a0538d4ba73ce4aa5eaf5966c
MathiasFaber <makr20ah@student.cbs.dk>
2024-11-18 20:33:21 +0100

    comments + cleanup

--- test3/src/Name/Properties.hs
+++ test3/src/Name/Properties.hs
@@ -32,25 +32,7 @@ strip a = do
 
 instance Stable Int
 
--- how do we make quickcheck work for polymorphic types
--- what are the considerations when generating clocks
--- totally random, at least one, overlap between?
--- should we maximise "clock tick" contention in some cases, minimize it in other cases
--- should we have a syncronous clock generator
--- should we have a constant clock generator
-
--- Property ideas for map. Specification for map:
--- => type constraint, arguments must derive typeclasses eq, arb and show
-
--- 1. Mapping id function is the signal itsself
--- 2. Mapping does not change the size of the signal, only content
--- 3. Mapping is assosciatve
--- 4. Causality??
-
 -- 1.
--- prop_map_id :: (Eq a, Arbitrary a, Show a) => Positive Int -> Sig a -> Bool
--- prop_map_id (Positive n) sig = eqSig n (map (box id) sig) sig
-
 prop_map_id :: (Eq a) => Sig a -> Bool
 prop_map_id sig = sig == map (box id) sig
 
@@ -77,32 +59,6 @@ prop_map_size :: Box (Int -> Int) -> Sig Int -> Bool
 prop_map_size f sig =
   sizeSig sig 0 == sizeSig (map f sig) 0
 
--- zip property ideas
-
--- 1.
--- zip with a constant empty signal is the signal itself
--- hacky by making stable int. why do we need this
-
--- -- could not deduce stable type a
-
--- prop_zip_const :: (Eq a, Arbitrary a, Show a) => Box (a -> a -> a) -> Sig a -> Bool
--- prop_zip_const sig = zip sig (0 ::: never) == sig
-
--- 2.
--- zip length increases with each tick of distinct streams by 2, except when they tick at the same time, in which case it only increases by one
--- this test is trivial untill we make an arbitrary clock generator
-
--- 3.
--- zip associativity hold for async streams zip A (zip A B) == zip (zip A B) A
-
--- could not deduce stable type a
-
--- prop_zip_associative :: (Eq a, Arbitrary a, Show a) => Box (a -> a -> a) -> Sig a -> Sig a -> Bool
--- prop_zip_associative f sig1 sig2 = zipWith f sig1 (zipWith f sig1 sig2) == zipWith f (zipWith f sig1 sig2) sig1
-
--- POTENTIALLY stuttering
--- either list 1 equals list 2
--- or list 2 is a stutter of list 1, where some element stutters 0 .. n times
 
 -- List version of isStuttering:
 prop_zip_isStuttering :: [Int] -> [Int] -> Bool
@@ -126,7 +82,7 @@ prop_zip_then_strip a b = do
   let stripped' = takeSigExhaustive stripped
   prop_zip_isStuttering a' stripped'
 
--- Signal version of isStuttering (experiment):
+-- Signal version of isStuttering:
 prop_zip_is_stuttering_sig :: Sig Int -> Sig Int -> Bool
 prop_zip_is_stuttering_sig a b = 
     prop_zip_is_stuttering_sig' a b 0
@@ -136,7 +92,11 @@ prop_zip_is_stuttering_sig' (x ::: Delay clx fx) (y ::: Delay cly fy) n
 
   -- if we've advanced on the original signal (x) twice without finding a match, we know its not a stuttering
   | n == 2 = False
+
+  -- if the original signals clock is null, it means the signal will never advance again
   | IntSet.null clx = False
+
+  -- if the stuttering signals clock is null, it must mean that we finished going through this signal, without returning false, and thereby the signal is a stuttering
   | IntSet.null cly = True
 
   -- if x and y are the same, we advance on the possible stuttering signal (y) and reset the n counter
@@ -150,16 +110,3 @@ prop_zip_then_strip_sig a b = do
   let zipped = prop_zip_zipped a b
   let stripped = strip zipped
   prop_zip_is_stuttering_sig a stripped
-
--- prop_zip_isStutteringSig :: Sig Int -> Sig Int -> Bool
--- prop_zip_isStutterignSig _
-
--- filter properties
-
--- 1. filter (const True) = id
-
--- 2. filter can only reduce or maintain the size of the signal; it never adds new elements.
-
--- 3. filter is imdepotent, subsequent filter calls on the same signal does not alter the signal
-
--- 4. filter composition
